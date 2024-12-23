579f35ca6f5f0fd995fd99bc0e568dbdbf112baf
SrLLamaDev <srllamadev@gmail.com>
2024-12-23 11:24:38 -0400

    a

--- archivo_prueba.py
+++ archivo_prueba.py
@@ -2,11 +2,8 @@ print ("comida")
 print ("sajta de pollo")
 print ("JHOYER :) ---->>>>>>>>>>> A QUE HORA EL VALORANT AJJAJAJAJA  <<<<---------")
 print ("yamil y sus seguidores")
-<<<<<<< HEAD
-int a=1,b=1;
-print(a+b);
+a=1
+b=1
+print(a+b)
 
 
-=======
-print ("xdxd")
->>>>>>> 2a8185e841531855b549a8d7b732bdd693d5351b
