e2be185593dffb53ea1a7102bd27ead0386221ae
SrLLamaDev <srllamadev@gmail.com>
2024-12-23 11:28:33 -0400

    aaaa

--- archivo_prueba.py
+++ archivo_prueba.py
@@ -1,23 +1,9 @@
 print ("comida")
 print ("sajta de pollo")
-<<<<<<< HEAD
-<<<<<<< HEAD
-q2|dgtfprint ("JHOYER :) ---->>>>>>>>>>> A QUE HORA EL VALORANT AJJAJAJAJA  <<<<---------")
-=======
 print ("yamil y sus seguidores")
->>>>>>> ae294ceb00b6c66e3a79124861a089243ed73fea
-
-
-
-=======
 print ("JHOYER :) ---->>>>>>>>>>> A QUE HORA EL VALORANT AJJAJAJAJA  <<<<---------")
 print ("yamil y sus seguidores")
 a=1
 b=1
 print(a+b)
-
-
-=======
 print ("xdxd")
->>>>>>> 2a8185e841531855b549a8d7b732bdd693d5351b
->>>>>>> b44c3a53494a4296a03ba8d8bdcaec2e8275358c
