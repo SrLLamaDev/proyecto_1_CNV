6110c8676ce19cb40454ddbfa8c3e2ec774f73b0
jhoyer123 <jhoyervega4@gmail.com>
2024-12-23 11:14:23 -0400

    que tal ...

--- archivo_prueba.py
+++ archivo_prueba.py
@@ -1,7 +1,8 @@
 print ("comida")
 print ("sajta de pollo")
-<<<<<<< HEAD
 print ("JHOYER :) ---->>>>>>>>>>> A QUE HORA EL VALORANT AJJAJAJAJA  <<<<---------")
-=======
 print ("yamil y sus seguidores")
->>>>>>> ae294ceb00b6c66e3a79124861a089243ed73fea
+int a=1,b=1;
+print(a+b);
+
+
